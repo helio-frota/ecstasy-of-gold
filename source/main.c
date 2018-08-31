7b1cb8be68f24daa362a0f91f98812f562c2e355
Helio Frota <00hf11@gmail.com>
2018-08-30 22:53:40 -0300

    src: force error message to see an error dialog * Also hard coded simplified version of cmake usage

--- source/main.c
+++ source/main.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
 
 int main()
 {
 
     ALLEGRO_DISPLAY *display = NULL;
 
-    if (!al_init())
+    if (1)
     {
-        printf("Failed to initialize allegro 5.\n");
-        return -1;
+        al_show_native_message_box(al_get_current_display(),
+                                 "EOG",
+                                 "Error:",
+                                 "Failed to initialize allegro 5.",
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        return EXIT_FAILURE;
     }
 
     display = al_create_display(800, 600);
 
     if (!display)
     {
-        printf("Failed to create display.\n");
-        return -1;
+        al_show_native_message_box(al_get_current_display(),
+                                 "EOG",
+                                 "Error:",
+                                 "Failed to create display.",
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        return EXIT_FAILURE;
     }
 
     // keep showing for 5 seconds only.
@@ -25,5 +34,5 @@ int main()
 
     al_destroy_display(display);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
