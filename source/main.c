21c0d4a4aa0d92812c3455b66208f1e5df793a39
Helio Frota <00hf11@gmail.com>
2018-08-31 21:39:33 -0300

    src: able to draw 4 rectangles in draft places

--- source/main.c
+++ source/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
-#include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_ttf.h>
 
 #define DISPLAY_WIDTH 1024
 #define DISPLAY_HEIGHT 768
+#define RECT_THICKNESS 4.0
 
 int main()
 {
@@ -21,10 +23,7 @@ int main()
     }
 
     al_set_new_display_flags(ALLEGRO_FULLSCREEN);
-
     ALLEGRO_DISPLAY *display = al_create_display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
-
-    al_set_window_title(display, "Ecstasy of gold");
 
     if (!display)
     {
@@ -38,10 +37,20 @@ int main()
 
     al_init_font_addon();
     al_init_ttf_addon();
+    al_init_primitives_addon();
+
+    al_set_window_title(display, "Ecstasy of gold");
+
+    ALLEGRO_COLOR blue = al_map_rgb(44, 117, 255);
 
     ALLEGRO_FONT *font = al_load_font("fonts/OpenSans-Regular.ttf", 36, 0);
-    al_draw_text(font, al_map_rgb(44, 117, 255), DISPLAY_WIDTH / 2,
-                 DISPLAY_HEIGHT / 2, ALLEGRO_ALIGN_CENTER, "Ecstasy of gold");
+    al_draw_text(font, blue, DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2,
+                 ALLEGRO_ALIGN_CENTER, "Ecstasy of gold");
+
+    al_draw_rectangle(20, 20, 200, 200, blue, RECT_THICKNESS);
+    al_draw_rectangle(20, 460, 200, 260, blue, RECT_THICKNESS);
+    al_draw_rectangle(DISPLAY_WIDTH - 20, 20, DISPLAY_WIDTH - 200, 200, blue, RECT_THICKNESS);
+    al_draw_rectangle(DISPLAY_WIDTH - 20, 460, DISPLAY_WIDTH - 200, 260, blue, RECT_THICKNESS);
 
     al_flip_display();
     al_rest(5.0);
